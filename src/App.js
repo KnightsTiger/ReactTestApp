c7d67d63025cd3033c03dd2f8760eecfb0622f0b
knight <tocaldera6734@gmail.com>
2019-03-24 16:51:40 +0530

    test 3

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React now
+            Learn any thing 
           </a>
         </header>
       </div>
